40f0b546fb0e56197ec5f2f08c3f75c8a5e18b42
priya-25sp <priyadharshini25325@gmail.com>
2025-05-05 12:12:21 +0530

    Create first_program.c

---
+++ first_program.c
@@ -0,0 +1,9 @@
+/***********************************
+   welcome to priyadharshini programming session.
+    05/05/2025 sankari
+    *************************************/
+#include <stdio.h>
+int main(){
+    printf("hlo priya");
+    return 0;
+}
