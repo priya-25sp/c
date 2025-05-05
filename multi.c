0f49350947027daf0f4728f7dbc356d0ce0761e1
priyadharshini <priyadharshini25325@gmail.com>
2025-05-05 13:03:28 +0530

    Add files via upload

---
+++ multi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    int multiplication;
+    printf("enter two numbers:");
+    scanf("%d %d", &a , &b);
+    multiplication= a*b;
+    printf("%d",multiplication);
+    return 0;
